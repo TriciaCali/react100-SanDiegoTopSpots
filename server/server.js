1b7c22f5c6a061c48b8bf12e4ad0d41a1e747c8e
Latricia Hunter <latricia@Latricias-iMac.local>
2022-05-31 18:29:00 -0700

    Finished Not Styled

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const axios = require('axios').default;
 const app = express();
 
 app.use(morgan('dev'));
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,38 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import TopSpot from './TopSpot';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+  componentWillMount() {
+    Axios
+        .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+        .then(response => response.data)
+        .then(topspots => this.setState({ topspots }));
+  }
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <h1>San Diego Top Spots</h1>
+        <h4>A list of the top 30 places to see in San Diego, California.</h4>
+        <div>{
+          this.state.topspots.map(topspot => (
+            <div><TopSpot
+              key={ topspot.id }
+              name={ topspot.name }
+              description={ topspot.description }
+              location={ topspot.location }
+            />
+         
+          </div>))
+}</div>
+      </div>
     );
   }
 }
-
-export default App;
